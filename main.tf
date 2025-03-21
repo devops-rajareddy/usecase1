2a94bfbd41af9ba826925c9c7fd1a2427048f082
vrajareddy564 <51180912+vrajareddy564@users.noreply.github.com>
2025-03-21 13:09:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "example" {
     name = "raja"
     location = "eastasia"
 }
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example1" {
     name = "raja2"
     location = "eastasia"
 }
