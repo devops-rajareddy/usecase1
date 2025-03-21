e38d42f94daa2fd042f62e56403e298e3ed78189
vrajareddy564 <51180912+vrajareddy564@users.noreply.github.com>
2025-03-21 12:47:15 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,5 +2,7 @@ provider "azurerm" {
     features {}
     
 subscription_id = "38bd44d5-d91c-4f34-8f1e-c946d6697ae4"
-
+client_id = "4cda14af-3c3b-4cf6-8473-e2669ce3f887"
+client_secret = "OYW8Q~IXF4GEydILe44B8L1gFfRt-NLYcFOG2dx4"
+tenant_id = "d0f66b2a-6d50-4e11-b7a6-e0fe38d06609"
 }
